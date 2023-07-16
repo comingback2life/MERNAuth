544132dce23a78474b1b8fa197fffc3e5f441eee
Samip Pokharel <callmesamip@gmail.com>
2023-07-16 20:03:03 +1000

    Implementing tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      body: ["Roboto", "sans-serif"],
+      mono: ["ui-monospace", "monospace"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
