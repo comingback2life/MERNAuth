da180e8e9750b88eacdad6723a4c4985b50ee718
Samip Pokharel <callmesamip@gmail.com>
2023-07-16 17:39:48 +1000

    Dotenv is still required

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
+import 'dotenv/config';
 import express from 'express';
-const PORT = process.env.PORT || 8000;
+
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.get('/', (req, res, next) => {
