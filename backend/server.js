327eb9a59d527475abe867eb98b371e88725d3a2
Samip Pokharel <callmesamip@gmail.com>
2023-07-18 01:22:05 +1000

    MonoRepo deplyoment failed, so deploying seperately

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,3 @@
-// import 'dotenv/config';
-import * as path from 'path';
 import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleWare.js';
@@ -14,17 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //for form-data
 app.use(cookieParser());
 app.use('/api/users', userRoutes);
-if (process.env.NODE_ENV === 'production') {
-    const __dirname = path.resolve();
-    app.use(express.static(path.join(__dirname, 'frontend/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    });
-} else {
-    app.get('/', (req, res, next) => {
-        res.send('Server is alive');
-    });
-}
+
+
+app.get('/', (req, res, next) => {
+    res.send('Server is alive');
+});
 app.use(notFound);
 app.use(errorHandler);
 userRoutes;
