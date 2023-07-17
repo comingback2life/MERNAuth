4751418efc3b30d25d539dacdab20524d16f165e
Samip Pokharel <callmesamip@gmail.com>
2023-07-18 08:44:09 +1000

    Added Cors

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,12 @@ import userRoutes from './routes/userRoutes.js';
 import { errorHandler, notFound } from './middleware/errorMiddleWare.js';
 import connectDB from './config/db.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 const PORT = process.env.PORT || 3000;
 connectDB();
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); //for form-data
 app.use(cookieParser());
